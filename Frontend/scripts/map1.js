750ac5549252d0e28567b4402ba5f97bedb8ae6b
JoseDelacruzs <jose.delacruzsnt@uanl.edu.mx>
2024-10-22 16:56:16 -0600

    Añadi la clase mapa1 y clase escena

---
+++ Frontend/scripts/map1.js
@@ -0,0 +1,78 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+export class Map {
+    constructor(scene, camera) {
+        this.camera = camera;
+        this.scene = scene;
+        this.skydomeMesh = null;
+        this.planeCollisionBox = null;
+        this.audioListener = new THREE.AudioListener();
+        this.backgroundMusic = new THREE.Audio(this.audioListener);
+
+        this.init();
+    }
+
+    init() {
+        this.loadStage();
+        this.addSkydome();
+        this.addPlane();
+    }
+
+    addSkydome() {
+        const skydomeGeometry = new THREE.SphereGeometry(500, 60, 40);
+        const skyTexture = new THREE.TextureLoader().load('./assets/images/cielo.png');
+        const skydomeMaterial = new THREE.MeshBasicMaterial({
+            map: skyTexture,
+            side: THREE.BackSide
+        });
+        this.skydomeMesh = new THREE.Mesh(skydomeGeometry, skydomeMaterial); // Almacena el skydome en una propiedad
+        this.scene.add(this.skydomeMesh);
+    }
+
+    addPlane() {
+        const planeGeometry = new THREE.PlaneGeometry(600, 600);
+        const edges = new THREE.EdgesGeometry(planeGeometry);
+        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff }); // Color blanco para las líneas
+        const planeLines = new THREE.LineSegments(edges, lineMaterial);
+        planeLines.rotation.x = -Math.PI / 2;
+        planeLines.position.y = 0.3; // Ajusta esto según sea necesario
+        this.scene.add(planeLines);
+        this.planeCollisionBox = new THREE.Box3().setFromObject(planeLines);
+    }
+
+    setupAudio() {
+        const audioLoader = new THREE.AudioLoader();
+        audioLoader.load('./assets/audio/backgroundMusic.mp3', (buffer) => {
+            this.backgroundMusic.setBuffer(buffer);
+            this.backgroundMusic.setLoop(true);
+            this.backgroundMusic.setVolume(0.5);
+            this.backgroundMusic.play();
+        });
+    }
+
+    loadStage() {
+        const loader = new GLTFLoader(); // O FBXLoader si tu modelo es FBX
+        loader.load(
+            '/Splash-Wars/Frontend/assets/models/scenarios/low_poly_city/city.glb', // Cambia esto a la ruta de tu modelo
+            (gltf) => {
+                console.log('Modelo cargado con exito', gltf)
+                const stage = gltf.scene; 
+                stage.scale.set(12, 12, 12);
+                stage.position.set(0, 0, 0); 
+                this.scene.add(stage); // Agrega el escenario a la escena
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded'); // Progreso de carga
+            },
+            (error) => {
+                console.error('An error happened', error); // Manejo de errores
+            }
+        );
+    }
+
+    update() {
+        // Actualiza la posición del audioListener según la posición de la cámara
+        this.audioListener.position.copy(this.camera.position);
+    }
+}
