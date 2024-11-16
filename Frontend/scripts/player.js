9a27cc99bb1ba7643647215899b3fa0890c18e89
Jesus De la Cruz <145614120+JoseDelacruzS@users.noreply.github.com>
2024-11-16 11:41:22 -0600

    Player nuevo y animaciones

--- Frontend/scripts/player.js
+++ Frontend/scripts/player.js
@@ -37,7 +37,7 @@ export class Player {
             '../assets/models/Player/source/little_boy_2.fbx',
             (object) => {
                 this.model = object;
-                this.model.scale.set(1, 1, 1);
+                this.model.scale.set(.03, .03, .03);
                 this.model.position.set(0, 1, 0);
                 this.loadTexture();
                 this.animations = new PlayerAnimations(this.model);
