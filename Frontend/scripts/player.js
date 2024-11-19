114ccbbb26f8dc434270706bf5211b2a8a602529
Jesus De la Cruz <145614120+JoseDelacruzS@users.noreply.github.com>
2024-11-19 04:18:57 -0600

    Visualizacion de jugadores

--- Frontend/scripts/player.js
+++ Frontend/scripts/player.js
@@ -81,7 +81,6 @@ export class Player {
             return;
         }
         this.model.position.set(position.x, position.y, position.z);
-        this.position.set(position.x, position.y, position.z);
     }
 
     setupKeyboardControls() {
