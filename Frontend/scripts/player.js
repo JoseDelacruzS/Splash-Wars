289cf9cca1373c9b11df795294eae7b09465e1c6
Jesus De la Cruz <145614120+JoseDelacruzS@users.noreply.github.com>
2024-11-19 06:40:22 -0600

    ,ultijugador implementado

--- Frontend/scripts/player.js
+++ Frontend/scripts/player.js
@@ -80,7 +80,7 @@ export class Player {
             console.warn('Intentando actualizar posición antes de cargar el modelo');
             return;
         }
-        this.position.set(position.x, position.y, position.z);
+        this.model.position.set(position.x, position.y, position.z);
     }
 
     setupKeyboardControls() {
